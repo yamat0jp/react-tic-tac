2038361f661b62d5224d8623f640bb37c072d501
yamat0jp <yamat0jp@yahoo.co.jp>
2022-10-25 17:30:05 +0900

    first commit tic-tac-toe

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,9 @@ import { Link } from 'gatsby'
 import { 
     container,
     heading,
-    navLinks,
     navLinkItem,
-    navLinkText } from './layout.module.css'
+    navLinkText,
+    gamePanel } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
     return (
@@ -31,5 +31,79 @@ const Layout = ({ pageTitle, children }) => {
         </div>
     )
 }
+
+const Panel = ({number}) => {
+    <li className={gamePanel}>{number}</li>
+}
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.player = 1
+        this.members = [0,0,0, 0,0,0, 0,0,0]
+        this.message = ''
+    }
+    move(member) {        
+        if (this.player === 0) {
+            this.player = 0
+            this.members = [0,0,0, 0,0,0, 0,0,0]
+            this.message = ''
+            return 
+        }
+        this.members[member] = this.player        
+        if (this.player === 1) 
+            this.player = 2
+        else 
+            this.player = 1        
+        if (this.judge(this.player)) {
+            if (this.player === 1) 
+                this.message = 'You win!!'
+            else 
+                this.message = 'Com win.'            
+        } else {
+            var Bool = false
+            for (var i in this.members) {
+                if (i !== 0) Bool = true                
+            }
+            if (Bool) {
+                this.message = 'Draw.'
+                this.player = 0
+            }
+        }
+    }
+    check(a,b,c) {
+        const p = this.player
+        if (this.members[a] === p && this.members[b] === p && this.members[c] === p) 
+            return true 
+        else return false        
+    }
+    judge(player) {
+        if (this.check(0,1,2) || this.check(3,4,5) || this.check(6,7,8) ||
+            this.check(0,3,6) || this.check(1,4,7) || this.check(2,5,8) ||
+            this.check(0,4,8) || this.check(2,4,6)) 
+            return true
+        else return false        
+    }
+    render() {
+        return (
+            <div>
+                <ul>
+                    <Panel number={this.members[0]}></Panel>
+                    <Panel number={this.members[1]}></Panel>
+                    <Panel number={this.members[2]}></Panel>
+                </ul>
+                    <Panel number={3}>3</Panel>
+                    <Panel number={4}></Panel>
+                    <Panel number={5}></Panel>
+                <ul>
+                    <Panel onClick={()=>this.move(3)}></Panel>
+                </ul>
+                <p>{this.message}</p>
+            </div>
+        )
+    }
+}
+
+export { Board }
 
 export default Layout

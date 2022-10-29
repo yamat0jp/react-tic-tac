76e2f892ef284d7f66acf78336a2730b2796e21b
yamat0jp <yamat0jp@yahoo.co.jp>
2022-10-29 10:49:46 +0900

    error が消えたようです

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,7 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import { 
-    container,
-    heading,
-    navLinkItem,
-    navLinkText,
+    container, heading, navLinkItem, navLinkText,
     gamePanel } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
@@ -14,64 +11,42 @@ const Layout = ({ pageTitle, children }) => {
                 <ul>
                     <li className={navLinkItem}>
                         <Link to="/" className={navLinkText}>
-                            Home
+                            <p>Home</p>
                         </Link>
                     </li>      
                     <li className={navLinkItem}>
                         <Link to="/about" className={navLinkText}>
-                            About
+                            <p>About</p>
                         </Link>
                     </li>
                 </ul>
             </nav>
             <main>
                 <h1 className={heading}>{pageTitle}</h1>
-                {children}
+                <p>{children}</p>
             </main>
         </div>
     )
 }
 
-const Board = () => {
-    var player = 1
-    var members = [0,0,0, 0,1,0, 0,0,0]
-    var message = ''
-    const Panel = ({number}) => 
-        <li className={gamePanel}>{display(number)}</li>   
-    function display ({num}) {
-        if (num === "1") return 'O'
-        else if (num === 2) return 'X'
-        else return null
+const Panel = ({player}) => {       
+    function display(p) {              
+        switch(p) {                        
+            case(1): 
+                return 'O'                
+            case(2): 
+                return 'X'                              
+            default:
+                return null                
+        }                        
     }
-    function move({member}) {            
-        if (player === 0) {
-            player = 1
-            members = [0,0,0, 0,0,0, 0,0,0]
-            message = ''
-            return 
-        }
-        members[member] = player        
-        if (player === 1) 
-            player = 2
-        else 
-            player = 1        
-        if (judge()) {
-            if (player === 1) 
-                message = 'You win!!'
-            else 
-                message = 'Com win.'            
-        } else {
-            var Bool = false
-            for (var i in members) {
-                if (i === 0) Bool = true                
-            }
-            if (Bool) {
-                message = 'Draw.'
-                player = 0
-            }            
-        }
-        return
-    } 
+    return <button className={gamePanel}>{display(player)}</button> 
+}
+
+const Board = () => {
+    const [player, setPlayer] = React.useState(1)
+    const [members, setMembers] = React.useState([0,0,0, 0,0,0, 0,0,0])
+    const [message, setMessage] = React.useState(null)
     function check({a,b,c}) {
         const p = player
         if (members[a] === p && members[b] === p && members[c] === p) 
@@ -81,23 +56,55 @@ const Board = () => {
     function judge() {
         if (check(0,1,2) || check(3,4,5) || check(6,7,8) ||
             check(0,3,6) || check(1,4,7) || check(2,5,8) ||
-            check(0,4,8) || check(2,4,6)) 
+            check(0,4,8) || check(2,4,6)) {            
             return true
-        else return false        
+        } else return false        
     } 
+    function move(index) {         
+        if (members[index] !== 0)             
+            return        
+        var dataCopy = members
+        dataCopy[index] = player
+        setMembers(dataCopy)         
+        if (player === 0) {
+            setPlayer(1)
+            setMembers([0,0,0, 0,0,0, 0,0,0])
+            setMessage(null)
+            return 
+        }
+        if (player === 1) 
+            setPlayer(2)
+        else 
+            setPlayer(1)        
+        if (judge()) {
+            if (player === 1) 
+                setMessage('You win!!')
+            else 
+                setMessage('Com win.')            
+        } else {
+            var Bool = false
+            for (var i in dataCopy) {
+                if (i === 0) Bool = true                
+            }
+            if (Bool) {
+                setMessage('Draw.')
+                setPlayer(0)                
+            }            
+        }           
+    }
     return (
-        <div>            
-            <Panel number={members[0]} onClick={()=>move(0)}></Panel>
-            <Panel number={members[1]} onClick={()=>move(1)}></Panel>
-            <Panel number={members[2]} onClick={()=>move(2)}></Panel>
+        <div className={container}>
+            <Panel player={members[0]} onClick={()=>move(0)}></Panel>
+            <Panel player={members[1]} onClick={()=>move(1)}></Panel>
+            <Panel player={members[2]} onClick={()=>move(2)}></Panel>
             <br />            
-            <Panel number={members[3]} onClick={()=>move(3)}></Panel>
-            <Panel number={members[4]} onClick={()=>move(4)}></Panel>
-            <Panel number={members[5]} onClick={()=>move(5)}></Panel>
+            <Panel player={members[3]} onClick={()=>move(3)}></Panel>
+            <Panel player={members[4]} onClick={()=>move(4)}></Panel>
+            <Panel player={members[5]} onClick={()=>move(5)}></Panel>
             <br />
-            <Panel number={members[6]} onClick={()=>move(6)}></Panel>
-            <Panel number={members[7]} onClick={()=>move(7)}></Panel>
-            <Panel number={members[8]} onClick={()=>alert("8")}></Panel>            
+            <Panel player={members[6]} onClick={()=>move(6)}></Panel>
+            <Panel player={members[7]} onClick={()=>move(7)}></Panel>
+            <Panel player={members[8]} onClick={()=>move(8)}></Panel>            
             <p>{message}</p>
         </div>
     )    
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,10 @@ import Board, { Layout } from '../components/layout'
 const IndexPage = () => {
     const s = "Let's play tic-tac-toe."
     return (
-      <Layout pageTitle="Home Page" children={s}><Board></Board></Layout>       
+      <>
+        <Layout pageTitle="Home Page">{s}</Layout>
+        <Board></Board>
+      </>
     )
 }
 

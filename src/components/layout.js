0e4f49678f56103df626a970ff48f284d0a9034e
yamat0jp <yamat0jp@yahoo.co.jp>
2022-10-30 19:53:45 +0900

    基礎が完成

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { collectedScriptsByPage, Link } from 'gatsby'
 import { 
     container, heading, navLinkItem, navLinkText,
     gamePanel } from './layout.module.css'
@@ -29,82 +29,81 @@ const Layout = ({ pageTitle, children }) => {
     )
 }
 
-const Panel = ({player}) => {       
-    function display(p) {              
-        switch(p) {                        
+const Board = () => {
+    const [player, setPlayer] = React.useState(1)
+    const [members, setMembers] = React.useState([0,0,0, 0,0,0, 0,0,0])
+    const [message, setMessage] = React.useState('start')    
+    function display(index) {              
+        switch(members[index]) {                        
             case(1): 
                 return 'O'                
             case(2): 
                 return 'X'                              
             default:
-                return null                
+                return '?'                
         }                        
     }
-    return <button className={gamePanel}>{display(player)}</button> 
-}
-
-const Board = () => {
-    const [player, setPlayer] = React.useState(1)
-    const [members, setMembers] = React.useState([0,0,0, 0,0,0, 0,0,0])
-    const [message, setMessage] = React.useState(null)
-    function check({a,b,c}) {
-        const p = player
-        if (members[a] === p && members[b] === p && members[c] === p) 
+    function check(a,b,c) {
+        const p = player 
+        if (members[a] === p && members[b] === p && members[c] === p) {            
             return true 
-        else return false        
+        } else return false        
     } 
-    function judge() {
+   function judge() {
         if (check(0,1,2) || check(3,4,5) || check(6,7,8) ||
             check(0,3,6) || check(1,4,7) || check(2,5,8) ||
             check(0,4,8) || check(2,4,6)) {            
             return true
         } else return false        
     } 
-    function move(index) {         
-        if (members[index] !== 0)             
-            return        
-        var dataCopy = members
-        dataCopy[index] = player
-        setMembers(dataCopy)         
+    function move(index) {          
         if (player === 0) {
             setPlayer(1)
             setMembers([0,0,0, 0,0,0, 0,0,0])
-            setMessage(null)
+            setMessage('restart')            
             return 
-        }
-        if (player === 1) 
-            setPlayer(2)
-        else 
-            setPlayer(1)        
-        if (judge()) {
+        }        
+       if (members[index] !== 0)             
+            return                
+        var dataCopy = members
+        dataCopy[index] = player
+        setMembers(dataCopy)         
+        if (judge()) {                    
             if (player === 1) 
                 setMessage('You win!!')
             else 
                 setMessage('Com win.')            
-        } else {
+            setPlayer(0)    
+            return        
+        } else {            
             var Bool = false
-            for (var i in dataCopy) {
-                if (i === 0) Bool = true                
-            }
-            if (Bool) {
-                setMessage('Draw.')
-                setPlayer(0)                
+            for (const i in dataCopy) {
+                if (dataCopy[i] === 0) Bool = true                                
             }            
-        }           
+            if (!Bool) {
+                setMessage('Draw.')
+                setPlayer(0)      
+                return          
+            }            
+        }      
+        if (player === 1) 
+            setPlayer(2)
+        else
+            setPlayer(1)
     }
     return (
         <div className={container}>
-            <Panel player={members[0]} onClick={()=>move(0)}></Panel>
-            <Panel player={members[1]} onClick={()=>move(1)}></Panel>
-            <Panel player={members[2]} onClick={()=>move(2)}></Panel>
+            <button className={gamePanel} onClick={()=>move(0)}>{display(0)}</button> 
+            <button className={gamePanel} onClick={()=>move(1)}>{display(1)}</button> 
+            <button className={gamePanel} onClick={()=>move(2)}>{display(2)}</button> 
             <br />            
-            <Panel player={members[3]} onClick={()=>move(3)}></Panel>
-            <Panel player={members[4]} onClick={()=>move(4)}></Panel>
-            <Panel player={members[5]} onClick={()=>move(5)}></Panel>
+            <button className={gamePanel} onClick={()=>move(3)}>{display(3)}</button> 
+            <button className={gamePanel} onClick={()=>move(4)}>{display(4)}</button> 
+            <button className={gamePanel} onClick={()=>move(5)}>{display(5)}</button> 
             <br />
-            <Panel player={members[6]} onClick={()=>move(6)}></Panel>
-            <Panel player={members[7]} onClick={()=>move(7)}></Panel>
-            <Panel player={members[8]} onClick={()=>move(8)}></Panel>            
+            <button className={gamePanel} onClick={()=>move(6)}>{display(6)}</button> 
+            <button className={gamePanel} onClick={()=>move(7)}>{display(7)}</button> 
+            <button className={gamePanel} onClick={()=>move(8)}>{display(8)}</button> 
             <p>{message}</p>
         </div>
     )    

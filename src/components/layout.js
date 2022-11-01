07b32065db17e579b0b96f69ec77a41a81baac36
yamat0jp <yamat0jp@yahoo.co.jp>
2022-11-01 18:12:57 +0900

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { collectedScriptsByPage, Link } from 'gatsby'
+import { Link } from 'gatsby'
 import { 
     container, heading, navLinkItem, navLinkText,
     gamePanel } from './layout.module.css'
@@ -33,6 +33,7 @@ const Board = () => {
     const [player, setPlayer] = React.useState(1)
     const [members, setMembers] = React.useState([0,0,0, 0,0,0, 0,0,0])
     const [message, setMessage] = React.useState('start')    
+    let man = player
     function display(index) {              
         switch(members[index]) {                        
             case(1): 
@@ -43,20 +44,27 @@ const Board = () => {
                 return '?'                
         }                        
     }
-    function check(a,b,c) {
-        const p = player 
+    function check(p,a,b,c) {        
         if (members[a] === p && members[b] === p && members[c] === p) {            
             return true 
         } else return false        
     } 
-   function judge() {
-        if (check(0,1,2) || check(3,4,5) || check(6,7,8) ||
-            check(0,3,6) || check(1,4,7) || check(2,5,8) ||
-            check(0,4,8) || check(2,4,6)) {            
+   function judge(local) {
+        if (check(local,0,1,2) || check(local,3,4,5) || check(local,6,7,8) ||
+            check(local,0,3,6) || check(local,1,4,7) || check(local,2,5,8) ||
+            check(local,0,4,8) || check(local,2,4,6)) {            
             return true
-        } else return false        
+        } else return false
     } 
-    function move(index) {          
+    function isEmpty() {
+        let test = false
+        for (const i in members) {
+            if (members[i] === 0) 
+                test = true
+        }           
+        return test
+    }
+    function move(index) {                  
         if (player === 0) {
             setPlayer(1)
             setMembers([0,0,0, 0,0,0, 0,0,0])
@@ -66,30 +74,66 @@ const Board = () => {
        if (members[index] !== 0)             
             return                
         var dataCopy = members
-        dataCopy[index] = player
+        dataCopy[index] = man
         setMembers(dataCopy)         
-        if (judge()) {                    
-            if (player === 1) 
+        if (judge(man)) {                    
+            if (man === 1) 
                 setMessage('You win!!')
-            else 
-                setMessage('Com win.')            
-            setPlayer(0)    
-            return        
-        } else {            
-            var Bool = false
-            for (const i in dataCopy) {
-                if (dataCopy[i] === 0) Bool = true                                
-            }            
-            if (!Bool) {
-                setMessage('Draw.')
-                setPlayer(0)      
-                return          
-            }            
-        }      
-        if (player === 1) 
-            setPlayer(2)
+            else
+                setMessage('Com win.')  
+            setPlayer(0) 
+            return                           
+        }                
+        if (!isEmpty()) {               
+            setMessage('Draw.')            
+            setPlayer(0)
+            return
+        }                        
+        console.log(man)        
+        man = next_p(man)                                           
+        if (man === 2)                                 
+            move(ai_move(man,1))                                           
+        return
+    }
+    function next_p(p) {
+        if (p === 1)
+            return 2
+        else    
+            return 1
+    }
+    function ai_move(local, level) {        
+        let min_max, tmp, num;
+        if (!isEmpty) return 0
+        if (local === man) 
+            min_max = -100
         else
-            setPlayer(1)
+            min_max = 100
+        for (const i in members) {
+            if (members[i] === 0)
+                members[i] = local
+            else    
+                continue
+            if (judge(local)) {
+                if (local === man)                
+                    tmp = 10 - level
+                else
+                    tmp = -(10-level)                
+            } else tmp = ai_move(next_p(local),level+1)    
+            if (local === man) {                                 
+                if (min_max < tmp) {
+                    min_max = tmp   
+                    num = i
+                }                                     
+            } else if (min_max > tmp) {
+                min_max = tmp
+                num = i
+            }
+            members[i] = 0
+        }
+        if (level === 1) {               
+            return num                        
+        } else    
+            return min_max
     }
     return (
         <div className={container}>
